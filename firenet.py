09a795482c5f5b99682fb2c99769a8a2eb307119
Jonas Gesch <s0564572@htw-berlin.de>
2023-05-09 19:46:03 +0200

    Add function for measuring runime
    This is for comparison against newer, optimized implementations.

--- firenet.py
+++ firenet.py
@@ -1,6 +1,7 @@
 import numpy as np
 from utils import Event, EventQueue
 from layers import ConvLayer
+from timeit import timeit
 
 
 KERNEL_SIZE = 3
@@ -13,23 +14,12 @@ SEG_WIDTH = 32
 SEG_HEIGHT = 32
 INPUT_EVENTS = 1000
 
-# initialise neuron states
-inputNeurons = np.zeros([CONV_CHANNELS, SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
-hiddenNeurons = np.zeros([6, CONV_CHANNELS, SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
-outputNeurons = np.zeros([SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
-
-# initialise kernel weights
-inputKernels = np.random.rand(KERNEL_NUM, INPUT_CHANNELS, KERNEL_SIZE, KERNEL_SIZE).astype(np.float16)-0.3
-hiddenKernels = np.random.rand(6, KERNEL_NUM, CONV_CHANNELS, KERNEL_SIZE, KERNEL_SIZE).astype(np.float16)-0.45
-#hiddenKernels[0] *= 0.5
-#hiddenKernels[1:] *= 0.0
-outputKernels = np.random.rand(OUTPUT_CHANNELS, CONV_CHANNELS, CONV_CHANNELS, 1, 1).astype(np.float16)*0.5
 
 # initialise event queues
 eventInput = EventQueue()
 
 # slider
-for i in range(SEG_HEIGHT*(SEG_WIDTH-1)):
+for i in range(SEG_HEIGHT//4*(SEG_WIDTH-1)):
     eventInput.put(Event(i//SEG_HEIGHT, i%SEG_HEIGHT, i//SEG_HEIGHT, 0))
     eventInput.put(Event(i//SEG_HEIGHT+1, i%SEG_HEIGHT, i//SEG_HEIGHT, 1))
 
@@ -39,19 +29,35 @@ for i in range(SEG_HEIGHT*(SEG_WIDTH-1)):
 #hiddenKernels[0,2,0,1] = [-55,55,-55]
 #hiddenNeurons[0,0,11,23:26] = [[0,1],[2,3],[55,10]]
 
-# init layers
-inputLayer = ConvLayer(2, len(hiddenKernels[0]), len(hiddenKernels[0, 0, 0]))
-convLayer = ConvLayer(CONV_CHANNELS, len(hiddenKernels[0]), len(hiddenKernels[0, 0, 0]))
+def inference(eventInput):
 
+    rng = np.random.default_rng(12)
 
-inputLayer.assignLayer(eventInput, inputKernels, inputNeurons, False)
-inputNeurons , ffQ = inputLayer.forward(None)
-print("%d spikes in input layer" %(ffQ.qsize()))
+    # initialise neuron states
+    inputNeurons = np.zeros([CONV_CHANNELS, SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
+    hiddenNeurons = np.zeros([6, CONV_CHANNELS, SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
+    outputNeurons = np.zeros([SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
 
-for l in range(HIDDEN_LAYERS):
-    convLayer.assignLayer(ffQ, hiddenKernels[l], hiddenNeurons[l], False)
-    hiddenNeurons[l], ffQ = convLayer.forward(None)
-    print("%d spikes in layer %d" %(ffQ.qsize(), l+1))
+    # initialise kernel weights
+    inputKernels = rng.random((KERNEL_NUM, INPUT_CHANNELS, KERNEL_SIZE, KERNEL_SIZE)).astype(np.float16)-0.48
+    hiddenKernels = rng.random((6, KERNEL_NUM, CONV_CHANNELS, KERNEL_SIZE, KERNEL_SIZE)).astype(np.float16)-0.5
+    outputKernels = rng.random((OUTPUT_CHANNELS, CONV_CHANNELS, CONV_CHANNELS, 1, 1)).astype(np.float16)*0.5
+
+    # init layers
+    inputLayer = ConvLayer(2, len(hiddenKernels[0]), len(hiddenKernels[0, 0, 0]))
+    convLayer = ConvLayer(CONV_CHANNELS, len(hiddenKernels[0]), len(hiddenKernels[0, 0, 0]))
+
+    inputLayer.assignLayer(eventInput, inputKernels, inputNeurons, False)
+    inputNeurons , ffQ = inputLayer.forward(None)
+    print("%d spikes in input layer" %(ffQ.qsize()))
+
+    for l in range(HIDDEN_LAYERS):
+        convLayer.assignLayer(ffQ, hiddenKernels[l], hiddenNeurons[l], False)
+        hiddenNeurons[l], ffQ = convLayer.forward(None)
+        print("%d spikes in layer %d" %(ffQ.qsize(), l+1))
+
+time = timeit(lambda: inference(eventInput), number=5)
+print(f"Time: {time:.6f}")
 
 #print(" c  x  y  t")
 # while outQ.qsize() > 0:
