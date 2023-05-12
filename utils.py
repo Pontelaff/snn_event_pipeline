2b779d4ce889d788d3ba563114227824b3f14c43
Jonas Gesch <jonas.gesch@dlr.de>
2023-05-12 17:26:06 +0200

    Add init function for SpikeQueue

--- utils.py
+++ utils.py
@@ -3,5 +3,8 @@ from collections import namedtuple
 Spike = namedtuple('Spike', ['x', 'y', 'c', 't'])
 
 class SpikeQueue(list):
+    def __init__(self, *args):
+        list.__init__(self, *args)
+
     def pop(self, i = 0) -> Spike:
         return super().pop()
