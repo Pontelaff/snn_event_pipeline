ec3fec1552afbc91d0ec83b98571ddefbe1b7bc1
Jonas Gesch <jonas.gesch@dlr.de>
2023-05-12 16:54:48 +0200

    Read network topology from loaded kernels

--- firenet.py
+++ firenet.py
@@ -8,21 +8,17 @@ MODEL_PATH = "pretrained/LIFFireNet.pth"
 INPUT_PATH = "datasets/cup-drop-short.h5"
 NUM_INPUT = 2000000
 
-CONV_CHANNELS = 32
-KERNEL_NUM = CONV_CHANNELS
-HIDDEN_LAYERS = 6
-INPUT_CHANNELS = 2
-OUTPUT_CHANNELS = 2
 SEG_WIDTH = 32
 SEG_HEIGHT = 32
 
-# initialise neuron states
-inputNeurons = np.zeros([CONV_CHANNELS, SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
-hiddenNeurons = np.zeros([6, CONV_CHANNELS, SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
-outputNeurons = np.zeros([SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
-
 # initialise kernel weights
 inputKernels, hiddenKernels, recKernels, outputKernels = loadKernels(MODEL_PATH)
+
+# initialise neuron states
+numHiddenLayers = len(hiddenKernels)
+inputNeurons = np.zeros([len(inputKernels), SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
+hiddenNeurons = np.zeros([numHiddenLayers, len(hiddenKernels[0]), SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
+outputNeurons = np.zeros([len(outputKernels), SEG_WIDTH, SEG_HEIGHT, 2], dtype=np.float16)
 
 # load input events from file
 eventInput = loadEvents(INPUT_PATH, NUM_INPUT)
@@ -30,15 +26,17 @@ eventInput = loadEvents(INPUT_PATH, NUM_INPUT)
 layerTimestamps = np.zeros(8)
 def inference(inputNeurons, hiddenNeurons, inputKernels, hiddenKernels, eventInput):
     # init layers
-    inputLayer = ConvLayer(2, len(hiddenKernels[0]), len(hiddenKernels[0, 0, 0]))
-    convLayer = ConvLayer(CONV_CHANNELS, len(hiddenKernels[0]), len(hiddenKernels[0, 0, 0]))
+    inputLayer = ConvLayer(len(inputKernels[0]), len(hiddenKernels[0]), len(hiddenKernels[0, 0, 0]))
+    convLayer = ConvLayer(len(hiddenKernels[0, 0]), len(hiddenKernels[0]), len(hiddenKernels[0, 0, 0]))
 
+    # run input layer
     inputLayer.assignLayer(eventInput, inputKernels, inputNeurons, layerTimestamps[0], False)
     inputNeurons , ffQ, layerTimestamps[0] = inputLayer.forward()
     print("%d spikes in input layer" %(len(ffQ)))
     num_spikes = len(ffQ)
 
-    for l in range(HIDDEN_LAYERS):
+    # run hidden layers
+    for l in range(numHiddenLayers):
         convLayer.assignLayer(ffQ, hiddenKernels[l], hiddenNeurons[l], layerTimestamps[l+1], False)
         hiddenNeurons[l], ffQ, layerTimestamps[l+1] = convLayer.forward()
         print("%d spikes in layer %d" %(len(ffQ), l+1))
