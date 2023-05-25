2edfaff2488d8e32da183503d6085719a1649b6e
Jonas Gesch <jonas.gesch@dlr.de>
2023-05-25 18:24:02 +0200

    Add function for visualizing neuron activity

--- visualization.py
+++ visualization.py
@@ -53,3 +53,102 @@ def plotNeuronActivity(neuronInputSpikes, neuronOutputSpikes):
     fig.suptitle('Spiking behaviour of a single neuron')
     # Display the figure
     plt.show()
+
+def compNeuronInput(pytorchArrPath, ownArrPath):
+    pytorch = np.load(pytorchArrPath)
+    pytorchSum = np.sum(pytorch, axis= (-1,-2))
+    own = np.load(ownArrPath)
+
+     # Create the figure and subplots
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
+
+    # Plot the stacked graph
+    # ax1.stackplot(range(num_bins), data, colors=plt.cm.viridis(np.linspace(0, 1, num_subgraphs)))
+    # ax1.set_xlim(0, num_bins)
+    # ax1.set_ylim(0, 25)
+    # ax1.set_ylabel('Weighted Input Spikes\nper Channel')
+
+
+    # Plot the pytorch input heatmap
+    im = ax1.imshow(np.transpose(pytorchSum), cmap='viridis', aspect='auto')
+    # Set the x and y-axis labels
+    #ax3.set_xlabel('Time Bins')
+    ax1.set_ylabel('Weighted Input Spikes\nper Channel')
+
+    # Add a colorbar
+    cbar = fig.colorbar(im, ax=ax1)
+    cbar.set_label('Input Current')
+    # Add a title to the plot
+    ax1.set_title('Pytorch Input Spike Heatmap')
+
+    # Plot the input heatmap of the own implementation
+    im = ax2.imshow(np.transpose(own), cmap='viridis', aspect='auto')
+    # Set the x and y-axis labels
+    #ax3.set_xlabel('Time Bins')
+    ax2.set_ylabel('Weighted Input Spikes\nper Channel')
+
+    # Add a colorbar
+    cbar = fig.colorbar(im, ax=ax2)
+    cbar.set_label('Input Current')
+    # Add a title to the plot
+    ax2.set_title('Own Input Spike Heatmap')
+
+
+    # Add a title to the figure
+    #fig.suptitle('Spiking behaviour of a single neuron')
+    # Display the figure
+    plt.show()
+
+
+def compNeuronLogs(pytorchArrInPath, ownArrInPath, pytorchArrOutPath, ownArrOutPath):
+    pytorchIn = np.load(pytorchArrInPath)
+    pytorchInSum = np.sum(pytorchIn, axis= (-1,-2))
+    pytorchOut = np.load(pytorchArrOutPath)[:-2, 1]
+    ownIn = np.load(ownArrInPath)
+    ownOut = np.load(ownArrOutPath)
+
+     # Create the figure and subplots
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 6))
+
+    # Plot the pytorch input heatmap
+    im = ax1.imshow(np.transpose(pytorchInSum), cmap='viridis', aspect='auto')
+    # Set the x and y-axis labels
+    #ax3.set_xlabel('Time Bins')
+    ax1.set_ylabel('Input Spikes\nper Channel')
+
+    # Add a colorbar
+    cbar = fig.colorbar(im, ax=ax1)
+    cbar.set_label('Input Current')
+    # Add a title to the plot
+    ax1.set_title('Pytorch Input Spike Heatmap')
+
+    # # Plot the input heatmap of the own implementation
+    # im = ax2.imshow(np.transpose(own), cmap='viridis', aspect='auto')
+    # # Set the x and y-axis labels
+    # #ax3.set_xlabel('Time Bins')
+    # ax2.set_ylabel('Weighted Input Spikes\nper Channel')
+
+    # # Add a colorbar
+    # cbar = fig.colorbar(im, ax=ax2)
+    # cbar.set_label('Input Current')
+    # # Add a title to the plot
+    # ax2.set_title('Own Input Spike Heatmap')
+
+    # Set the width and position of the bars
+    bar_width = 0.35
+    num_bins = len(pytorchOut)
+    bar_positions = np.arange(num_bins)
+
+    # Plot the output graph
+    ax2.bar(bar_positions - bar_width/2, pytorchOut, color='red', alpha=0.7, width=bar_width, label='Pytorch (batch based)')
+    ax2.bar(bar_positions + bar_width/2, ownOut, color='blue', alpha=0.7, width=bar_width, label='Own (event based)')
+    ax2.set_xlim(0, num_bins)
+    ax2.set_ylim(0, 10)
+    ax2.set_xlabel('Time Bins')
+    ax2.set_ylabel('Output Spikes')
+    ax2.legend()
+
+    # Add a title to the figure
+    #fig.suptitle('Spiking behaviour of a single neuron')
+    # Display the figure
+    plt.show()
